32727ecc55705cdf58897ebdd6cd68dc7e755ef6
hpenney2 <hpenney1010@gmail.com>
2020-08-02 20:44:56 -0400

    Added config.json generator
    Also has Reddit fields to future-proof for PRAW

---
+++ gen_config.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+if os.path.exists("config.json"):
+    print("config.json already exists! If you'd like to make an new one, please remove the current one first.")
+    exit(1)
+
+data = {}
+
+print("Enter your bot token:")
+token = input()
+data["token"] = token
+
+print("Enter your GitHub username:")
+username = input()
+data["githubUser"] = username
+
+print("Enter your GitHub personal access token (requires repo scope):")
+pat = input()
+data["githubPAT"] = pat
+
+print("Enter your Reddit client ID:")
+clientID = input()
+data["redditCID"] = clientID
+
+print("Enter your Reddit client secret:")
+secret = input()
+data["redditSecret"] = secret
+
+with open("config.json", "w") as config:
+    json.dump(data, config)
+print("config.json generated successfully!")
