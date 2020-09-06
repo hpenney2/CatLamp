8aba9059e59d84b56fa297fa141ce4033f8433e1
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2020-09-06 13:51:04 -0400

    what the fuck what the fuck what the fuck

--- cogs/commands/images.py
+++ cogs/commands/images.py
@@ -4,6 +4,7 @@ from PIL import Image
 import io
 import discord
 from discord.ext import commands
+import random
 
 
 async def getImage(ctx, user: discord.User = None):
@@ -49,10 +50,45 @@ def hippityHoppityThisColorIsDisappearity(img: Image.Image, color: tuple = (0, 2
     return img
 
 
+def replaceColor(image: Image.Image, targetIn: tuple, colorOut: tuple):
+    image = hippityHoppityThisColorIsDisappearity(image, targetIn)
+    result = Image.new(mode=image.mode, size=(image.width, image.height), color=colorOut)
+    result.paste(image)
+    return result
+
+
+def findAlphaTarget(image1: Image.Image, image2: Image.Image):
+
+    satisfied = False
+    potentialTarget = (0, 255, 0, 255)  # start with green
+    blacklist = []
+    fuck = 0
+
+    while not satisfied:
+        if fuck == 10:
+            random.seed()
+            fuck = 0
+
+        if potentialTarget not in blacklist:
+            if potentialTarget in image1.getdata() or potentialTarget in image2.getdata():
+                blacklist.append(potentialTarget)
+            else:
+                satisfied = True
+        else:
+            fuck += 1
+
+        # randomize target
+        potentialTarget = (random.randrange(0, 256), random.randrange(0, 256), random.randrange(0, 256), 255)
+
+    print(potentialTarget)
+    return potentialTarget
+
+
 class Images(commands.Cog, name="Image Manipulation"):
     def __init__(self, bot):
         self.bot = bot
         self.bot.cmds.append(self.deepfry)
+        self.catLampTemplate = Image.open('catlamp-outlineonly.png', mode='r').convert('RGBA')
 
     @commands.command(cooldown_after_parsing=True)
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -71,25 +107,26 @@ class Images(commands.Cog, name="Image Manipulation"):
     async def catLamp(self, ctx, user: discord.User = None):
         """catlamp here"""
         async with ctx.channel.typing():
+            # set the images
             image = await getImage(ctx, user)
-            # size = 870, 870  # the resolutions need to match
-            # image.thumbnail(size)
-            print(image.size)
+            overlay = self.catLampTemplate
 
-            template = Image.open('catlamp-outlineonly.png', mode='r')
-
+            # convert the images to be equal in size and mode for compatibility
             image = forceSquare(image)
-            print(image.size, template.size)
-            if image.size > template.size:
-                image.thumbnail(template.size)
+            if image.size > overlay.size:
+                image.thumbnail(overlay.size)
             else:
-                template.thumbnail(image.size)
+                overlay.thumbnail(image.size)
+            image = image.convert(mode=overlay.mode)
 
-            image = image.convert(mode=template.mode)
+            # find a color not in either image so we can use it for transparency in the final product
+            alpha = findAlphaTarget(image, overlay)
 
-            print(image.size == template.size, template.mode == image.mode)
-            outImg = Image.alpha_composite(image, template)  # processing here
-            outImg = hippityHoppityThisColorIsDisappearity(outImg)
+            # set alpha color outside of the lamp
+            overlay = replaceColor(overlay, (0, 255, 0, 255), alpha)
+
+            # combine catLamp with image
+            outImg = overlay#Image.alpha_composite(image, overlay)
 
             img = io.BytesIO()
             outImg.save(img, "png")
