f77a0c0431f1f80d7c7f3d39405ea385e4f4a031
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2020-09-11 15:19:09 -0400

    pspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspspsps

--- cogs/listeners/message.py
+++ cogs/listeners/message.py
@@ -9,8 +9,10 @@ class Message(commands.Cog):
     async def on_message(self, msg):
         if msg.author.id == self.bot.user.id or msg.author.bot:
             return
-        if msg.content != "python" and msg.content.lower().startswith("do not the sex"):
+        if msg.content != "python" and "do not the sex" in msg.content.lower():
             await msg.channel.send("do not the sex")
+        elif msg.content != "python" and "psps" in msg.content.lower():
+            await msg.add_reaction('<:lampstare:747483819388436570>')
 
 
 def setup(bot):
