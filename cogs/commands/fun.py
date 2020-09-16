97683ff2eac3796590491ea7689d5504ec69ed1e
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2020-09-16 13:42:33 -0400

    reshuffle the code to fix things, comment out unfinished check/cleanup function

--- cogs/commands/fun.py
+++ cogs/commands/fun.py
@@ -19,43 +19,16 @@ async def sendPost(ctx, post):
     # remove problematic &#x200B; that fuck with link detection
     if randPost.selftext.startswith("&#x200B;\n"):
         embed.description = randPost.selftext.replace("&#x200B;\n", "").strip('\n')
+        fuckYouX200B = True
+    else:
+        fuckYouX200B = False
 
     footerNote = None
-    checkImage = False
+
     if randPost.url and not randPost.is_self:
-        expressions = []
-        for i in ['img', 'image', 'gif', 'g.f', 'gf']:
-            for ex in regex.findall(i, randPost.url):
-                expressions.append(ex)
-        if len(expressions) > 0:
-            checkImage = True
-        if randPost.url[-4:] in ('.gif', '.png', '.jpg', 'jpeg'):
-            embed.set_image(url=randPost.url)
-        elif randPost.url.startswith("https://v.redd.it/") or randPost.url[-4:] in ('gifv', '.mp4',
-                                                                                    'webm', 'webp'):
-            embed.description = f"[(Video)]({randPost.url})"
-            footerNote = 'This is a video, which is not supported in Discord bot embeds.'
-            # Currently, it's impossible to add custom videos to embeds, so this is my solution for now.
-        elif randPost.url.startswith("/r/"):
-            embed.description = f"[(Crosspost)](https://www.reddit.com{randPost.url})"
-        elif not randPost.url.startswith("https://i.redd.it/"):
-            if checkImage:
-                badSite = None
-                for i in ["https://gfycat.com", "https://redgifs.com", "https://imgur.com"]:
-                    if randPost.url.startswith(i):
-                        badSite = i
-                if badSite:
-                    embed.description = f"[(GIF)]({randPost.url})"
-                    footerNote = f'This GIF is on {badSite},' \
-                                 f' which is too complex for Discord bot embeds.'
-            if randPost.url.startswith('https://www.reddit.com/gallery/'):
-                footerNote = 'This is a Reddit Gallery, which is impossible to format into one ' \
-                             'embed.'
-            embed.description = f"[(Link)]({randPost.url})"
-    else:
-        if embed.description.startswith('https://preview.redd.it/'):
-            embed.set_image(url=randPost.selftext.replace("&#x200B;\n", "").strip('\n'))
-            embed.description = None
+        embed, footerNote = urlParse(randPost.url, embed)
+    elif fuckYouX200B:
+        embed, footerNote = urlParse(embed.description, embed)
     try:
         embed.set_author(name=f"Posted by /u/{randPost.author.name}")
     except AttributeError:
@@ -68,6 +41,53 @@ async def sendPost(ctx, post):
 
     await ctx.send(embed=embed)
     return True
+
+
+def urlParse(url: str, embed: discord.Embed):
+    footerNote = ''
+    checkImage = False
+    expressions = []
+    for i in ['img', 'image', 'gif', 'g.f', 'gf']:
+        for ex in regex.findall(i, url):
+            expressions.append(ex)
+    if len(expressions) > 0:
+        checkImage = True
+    if url[-4:] in ('.gif', '.png', '.jpg', 'jpeg'):
+        embed.set_image(url=url)
+    if url.startswith("https://v.redd.it/") or url[-4:] in ('gifv', '.mp4',
+                                                            'webm', 'webp'):
+        embed.description = f"[(Video)]({url})"
+        footerNote = 'This is a video, which is not supported in Discord bot embeds.'
+        # Currently, it's impossible to add custom videos to embeds, so this is my solution for now.
+    elif url.startswith("/r/"):
+        embed.description = f"[(Crosspost)](https://www.reddit.com{url})"
+    if not url.startswith("https://i.redd.it/"):
+        if checkImage:
+            badSite = None
+            for i in ["https://gfycat.com", "https://redgifs.com", "https://imgur.com"]:
+                if url.startswith(i):
+                    badSite = i
+            if badSite:
+                embed.description = f"[(GIF)]({url})"
+                footerNote = f'This GIF is on {badSite},' \
+                             f' which is too complex for Discord bot embeds.'
+        if url.startswith('https://www.reddit.com/gallery/'):
+            footerNote = 'This is a Reddit Gallery, which is impossible to format into one ' \
+                         'embed.'
+        embed.description = f"[(Link)]({url})"
+    if embed.description.startswith('https://preview.redd.it/'):
+        embed.set_image(url=url)
+        embed.description = None
+
+    # try:
+    #     if embed.description.startswith('http') and embed.image != discord.Embed.Empty:
+    #         print('link to image, delete desc')
+    #         embed.description = ''
+    #     elif not embed.description.startswith('http') and embed.image != discord.embeds.EmbedProxy:
+    # except AttributeError:  # ok there is no description, so just ignore lol
+    #     pass
+
+    return embed, footerNote
 
 
 class Fun(commands.Cog):
