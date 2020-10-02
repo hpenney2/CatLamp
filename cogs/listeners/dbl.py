c2207afa26f1424c00eef06fb1b510d5aba2a33b
hpenney2 <hpenney1010@gmail.com>
2020-10-02 11:02:52 -0500

    fix DBL cog because it pushed an old file

--- cogs/listeners/dbl.py
+++ cogs/listeners/dbl.py
@@ -1,7 +1,12 @@
 import dbl
 import discord
 from discord.ext import commands
+from CatLampPY import config
 
 class DBL(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.dblpy = dbl.DBLClient(self.bot, config["dblToken"], autopost=True)
+
+def setup(bot):
+    bot.add_cog(DBL(bot))
