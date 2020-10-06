bfd2866890339a6aae19871264e770147c6a9282
hpenney2 <hpenney1010@gmail.com>
2020-10-06 11:31:29 -0500

    or don't set the status codes because now it's empty responses
    This reverts commit ea12ba4f82056b624a251b3814fe4dc903e8f72c.

--- cogs/listeners/dbl.py
+++ cogs/listeners/dbl.py
@@ -17,12 +17,12 @@ class DBL(commands.Cog):
             auth = request.headers.get("Authorization")
             if auth != "Imagin3ActuallyHavingGo0dSEcuR1tyF0rCATLAMP":
                 print(f"DBL POST request recieved with invalid Authorization header!!!\nAuth header recieved: {auth}")
-                return web.json_response({"error": "invalid_auth_header"}).set_status(400)
+                return web.json_response({"error": "invalid_auth_header"})
             else:
                 ctype = request.headers.get("Content-Type")
                 if ctype != "application/json":
                     print(f"DBL POST request failed due to not having a JSON content type!\nContent-Type header: {ctype}")
-                    return web.json_response({"error": "bad_content_type"}).set_status(400)
+                    return web.json_response({"error": "bad_content_type"})
                 botlogs = self.bot.get_channel(712489826330345534)
                 post = await request.json()
                 postType = None
@@ -30,7 +30,7 @@ class DBL(commands.Cog):
                     postType = post["type"]
                 except KeyError:
                     print("'type' key missing from request body!")
-                    return web.json_response({"error":"missing_keys"}).set_status(400)
+                    return web.json_response({"error":"missing_keys"})
                 if postType == "test":
                     print("Hello, world! Webhook test received from DBL.")
                     await botlogs.send("Hello, world! Webhook test received from DBL.")
@@ -47,7 +47,7 @@ class DBL(commands.Cog):
                 else:
                     print(f"Unknown post type! Got '{postType}', expected 'upvote' or 'test'")
                     await botlogs.send(f"Unknown post type! Got '{postType}', expected 'upvote' or 'test'")
-                    return web.json_response({"error":"bad_vote_type"}).set_status(400)
+                    return web.json_response({"error":"bad_vote_type"})
         app = web.Application()
         app.add_routes([web.post("/dblwebhook", handleDBLVote),
                         web.post("/dblwebhook/", handleDBLVote)])
