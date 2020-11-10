b9caf3c7afd134634f2453338228aed86477e444
TheEgghead27 <45245358+TheEgghead27@users.noreply.github.com>
2020-11-10 15:26:08 -0500

    capital PSPSPPSPSPSPSPPS

--- cogs/listeners/message.py
+++ cogs/listeners/message.py
@@ -1,4 +1,6 @@
 from discord.ext import commands
+import re as regex
+import datetime
 
 
 def isOk(msg):
@@ -11,17 +13,40 @@ def isOk(msg):
 class Message(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.crappyCapsDetector = regex.compile("[P, S][p,s]|[p, s][P, S]")  # i gotta be honest
 
     @commands.Cog.listener()
     async def on_message(self, msg):
+        start = datetime.datetime.now()
         if isOk(msg):
-            if msg.author.id == self.bot.user.id or msg.author.bot:
+            if msg.author.id == self.bot.user.id or msg.author.bot or msg.content == "python" :
                 return
-            if msg.content != "python" and "do not the sex" in msg.content.lower():
+
+            if "do not the sex" in msg.content.lower():
                 await msg.channel.send("do not the sex")
-            elif msg.content != "python" and "do the sex" in msg.content.lower():
+            elif "do the sex" in msg.content.lower():
                 await msg.channel.send("do **not** the sex")
-            if msg.content != "python" and "psps" in msg.content.lower():
+            if "psps" in msg.content.lower():
+                # piss counter
+                piss = 0
+                for _ in regex.findall("ps", msg.content.lower()):
+                    piss += 1
+
+                # full piss counter
+                capitals = 0
+                for _ in regex.findall("PS", msg.content):
+                    capitals += 2
+                if capitals / 2 == piss:  # if it's all PS, let it w i d e
+                    print('piss')
+                    piss = 3
+
+                if piss >= 3:
+                    for _ in regex.findall(self.crappyCapsDetector, msg.content):
+                        capitals += 1
+
+                    if capitals >= 3:
+                        await msg.add_reaction('<:lampstarenear:775818082534424577>')
+                        return
                 await msg.add_reaction('<:lampstare:747483819388436570>')
 
 
