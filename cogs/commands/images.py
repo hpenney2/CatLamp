380fe6512fec01725509cdf5fe451d08e05a0496
hpenney2 <hpenney1010@gmail.com>
2020-12-03 15:34:45 -0600

    fix conflict, merged master

--- cogs/commands/images.py
+++ cogs/commands/images.py
@@ -484,41 +484,6 @@ class Images(commands.Cog, name="Image Manipulation"):
 
             await sendImage(ctx, outImg, "rotate.png")
 
-    @commands.command(cooldown_after_parsing=True, aliases=["xFlip", "sideFlip"])
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    async def mirror(self, ctx, *, user: discord.Member = None):
-        """Creates a mirrored image of the attached image or your/the mentioned user's avatar."""
-        async with ctx.channel.typing():
-            image = await getImage(ctx, user)
-
-            outImg = image.transpose(method=Image.FLIP_LEFT_RIGHT)  # processing here
-
-            await sendImage(ctx, outImg, "mirror.png")
-
-    @commands.command(cooldown_after_parsing=True, aliases=["yFlip", "topFlip", "bottomFlip"])
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    async def flip(self, ctx, *, user: discord.Member = None):
-        """Creates an upside-down copy of the attached image or your/the mentioned user's avatar."""
-        async with ctx.channel.typing():
-            image = await getImage(ctx, user)
-
-            outImg = image.transpose(method=Image.FLIP_TOP_BOTTOM)  # processing here
-            outImg = outImg.transpose(method=Image.FLIP_LEFT_RIGHT)  # top bottom makes it also flip on the x
-
-            await sendImage(ctx, outImg, "upside_down.png")
-
-    @commands.command(cooldown_after_parsing=True)
-    @commands.cooldown(1, 5, commands.BucketType.user)
-    async def rotate(self, ctx, degrees: float, user: discord.Member = None):
-        """Rotates the attached image or your/the mentioned user's avatar
-        clockwise by the specified number of degrees."""
-        async with ctx.channel.typing():
-            image = await getImage(ctx, user)
-
-            outImg = image.rotate(angle=-degrees)  # for some cursed reason, rotate() defaults to counterclockwise
-
-            await sendImage(ctx, outImg, "rotate.png")
-
 
 def setup(bot):
     bot.add_cog(Images(bot))
